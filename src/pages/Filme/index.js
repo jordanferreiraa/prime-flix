dd18c2c32e8fe2aa3ac66322e704442f1e278f12
jordanferreiraa <jordanferreira69@gmail.com>
2023-08-17 22:52:43 -0300

    feat: detalhes do filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,66 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./style.css"
+import api from "../../services/api";
+
 function Filmes() {
-  return ( 
-    <h1>Bem vindo a pagina detalhes do filme</h1>
-   );
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState({});
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api.get(`/movie/${id}`, {
+        params: {
+          api_key: "097d3a03886efefeb5fba5ae3fa0e616",
+          language: "pt-BR", 
+        }
+      })
+      .then((response) => {
+        setFilme(response.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        console.log("FILME NÃO ENCONTRADO")
+      })
+    }
+
+    loadFilme();
+
+
+    return () => {
+      console.log("COMPONENTE FOI DESMONTADO")
+    }
+  }, [])
+
+  if(loading){
+    return(
+      <div className="filme-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    )
+  }
+
+  return( 
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+      <h3>Sinoptse</h3>
+      <span>{filme.overview}</span>
+      <strong>Avaliação: {filme.vote_average.toFixed(1)} / 10</strong>
+
+      <div className="area-buttons">
+        <button>Salvar</button>
+        <button>
+          <a href="#">
+            Trailer
+          </a>
+        </button>
+      </div>
+
+    </div>
+  );
 }
 
 export default Filmes;
