3ff25a9754d10c5b91f9be5fcf1e66c4ee35f717
jordanferreiraa <jordanferreira69@gmail.com>
2023-08-21 17:01:28 -0300

    feat: delete movie

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -14,9 +14,22 @@ function Favoritos() {
 
   }, [])
 
+  function excluirFilme(id) {
+    let filtroFilmes = filmes.filter((item) => {
+      return (item.id !== id)
+    })
+
+    setFilmes(filtroFilmes)
+    localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes))
+  }
+
   return( 
     <div className="meus-filmes">
       <h1>Meus filmes</h1>
+
+      {
+        filmes.length === 0 && <span>Você não possui nenhum filme salvo! </span>
+      }
 
       <ul>
         {
@@ -27,7 +40,7 @@ function Favoritos() {
 
                 <div>
                   <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                  <button>Excluir</button>
+                  <button onClick={ () => excluirFilme(item.id) }>Excluir</button>
                 </div>
               </li>
             )
